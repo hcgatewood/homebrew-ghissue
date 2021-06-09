9f89ec8fb89e1cbc5de874a1489d80fd3fe95fdd
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-08 20:11:06 -0700

    Brew formula update for ghissue version v0.1.5

--- ghissue.rb
+++ ghissue.rb
@@ -5,24 +5,24 @@
 class Ghissue < Formula
   desc ""
   homepage "https://github.com/hcgatewood/ghissue"
-  version "0.1.4"
+  version "0.1.5"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/hcgatewood/ghissue/releases/download/v0.1.4/ghissue_0.1.4_Darwin_x86_64.tar.gz"
-      sha256 "204cfc29bbc938ecc76976b99b80448fac7c7412dc8896e121940300424f61bb"
+      url "https://github.com/hcgatewood/ghissue/releases/download/v0.1.5/ghissue_0.1.5_Darwin_x86_64.tar.gz"
+      sha256 "bb8f43c84fa5ec46caa14c4a80ccff19c0db2a1461a5994f6c049055d612eda8"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/hcgatewood/ghissue/releases/download/v0.1.4/ghissue_0.1.4_Linux_x86_64.tar.gz"
-      sha256 "174433bba5c5991101c80305315c60202e0699b76c209241f632885520661ea1"
+      url "https://github.com/hcgatewood/ghissue/releases/download/v0.1.5/ghissue_0.1.5_Linux_x86_64.tar.gz"
+      sha256 "97a98b5f09ff1ba664ba6a35d3d224295d2c00c614a6eb03670fae222202d98c"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/hcgatewood/ghissue/releases/download/v0.1.4/ghissue_0.1.4_Linux_arm64.tar.gz"
-      sha256 "da309dea9ead96da25b099d217829def917752435ff8f31991055418c43232c4"
+      url "https://github.com/hcgatewood/ghissue/releases/download/v0.1.5/ghissue_0.1.5_Linux_arm64.tar.gz"
+      sha256 "25838f44276b4bc2b534ea2acebe1baec0b6ef56cba6f8556f6bbfdc2b6c72d7"
     end
   end
 
